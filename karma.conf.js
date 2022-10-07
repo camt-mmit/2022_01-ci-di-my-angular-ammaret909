bfa5a2703775fa75b6866ef9c939cf84040d9dc4
Kaw <ammaret_aituphon@cmu.ac.th>
2022-10-07 11:22:02 +0700

    feature-01

--- karma.conf.js
+++ karma.conf.js
@@ -37,6 +37,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    browsers: ["Chrome", "chromeHeadless", "ChromeHeadlessCI"],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"],
+      },
+    },
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
